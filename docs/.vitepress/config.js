8479b93c2fe739a0ee59164b66c662114a46a340
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-07-12 09:43:17 +0800

    Change repo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,16 +1,19 @@
 module.exports = {
-	title: 'VitePress template',
-	description: 'A JAMstack website template with the VitePress and Netlify CMS config.',
-	themeConfig: {
-		repo: 'tsutoringo/VitePress-with-Netlify-CMS',
-		docsDir: 'docs',
-		editLinks: true,
-		editLinkText: 'Edit this page on GitHub',
-		lastUpdated: 'Last Updated',
-		nav: [
-			{ text: 'Home', link: '/' },
-			{ text: 'Guide', link: '/guide/' },
-			{ text: 'GitHub', link: 'https://github.com/tsutoringo/VitePress-with-Netlify-CMS' }
-		],
-	}
+  title: "APS Alat Berat",
+  description: "Persiapan Akreditasi Program Studi D-III Alat Berat Poltekba.",
+  themeConfig: {
+    repo: "yasminzy/apsab",
+    docsDir: "docs",
+    editLinks: true,
+    editLinkText: "Edit this page on GitHub",
+    lastUpdated: "Last Updated",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      {
+        text: "GitHub",
+        link: "https://github.com/yasminzy/apsab"
+      }
+    ]
+  }
 }
