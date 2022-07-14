8d9a6acb6fc062ecc3d265354e0670b92eb4fd8c
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-07-14 09:43:30 +0800

    Edit config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,19 +1,113 @@
 module.exports = {
-  title: "APS Alat Berat",
-  description: "Persiapan Akreditasi Program Studi D-III Alat Berat Poltekba.",
+  description: "Kinerja dan evaluasi diri prodi D-III Alat Berat Poltekba",
+  lang: "id",
+  lastUpdated: true,
+  markdown: {
+    theme: "nord"
+  },
+  title: "APS Alat Berat Poltekba",
+  titleTemplate: false,
+
   themeConfig: {
-    repo: "yasminzy/apsab",
-    docsDir: "docs",
-    editLinks: true,
-    editLinkText: "Edit this page on GitHub",
-    lastUpdated: "Last Updated",
+    logo: "/media/logo.svg",
+    siteTitle: false,
+
     nav: [
-      { text: "Home", link: "/" },
-      { text: "Guide", link: "/guide/" },
       {
-        text: "GitHub",
-        link: "https://github.com/yasminzy/apsab"
-      }
-    ]
+        text: "Data",
+        link: "/data/kondisi-eksternal",
+        activeMatch: "/data/"
+      },
+      { text: "Admin", link: "/admin/" }
+    ],
+
+    sidebar: {
+      "/data/": [
+        {
+          text: "Pembuka",
+          collapsible: true,
+          items: [
+            { text: "Kondisi Eksternal", link: "/data/kondisi-eksternal" },
+            {
+              text: "Profil UPPS",
+              link: "/data/profil-unit-pengelola-program-studi"
+            }
+          ]
+        },
+        {
+          text: "Kriteria Akreditasi",
+          collapsible: true,
+          items: [
+            {
+              text: "Visi, Misi, Tujuan dan Strategi",
+              link: "/data/kriteria-1-visi-misi-tujuan-dan-strategi"
+            },
+            {
+              text: "Tata Pamong, Tata Kelola dan Kerja Sama",
+              link: "/data/kriteria-2-tata-pamong-tata-kelola-dan-kerja-sama"
+            },
+            {
+              text: "Mahasiswa",
+              link: "/data/kriteria-3-mahasiswa"
+            },
+            {
+              text: "Sumber Daya Manusia",
+              link: "/data/kriteria-4-sumber-daya-manusia"
+            },
+            {
+              text: "Keuangan, Sarana dan Prasarana",
+              link: "/data/kriteria-5-keuangan-sarana-dan-prasarana"
+            },
+            {
+              text: "Pendidikan",
+              link: "/data/kriteria-6-pendidikan"
+            },
+            {
+              text: "Penelitian",
+              link: "/data/kriteria-7-penelitian"
+            },
+            {
+              text: "Pengabdian kepada Masyarakat",
+              link: "/data/kriteria-8-pengabdian-kepada-masyarakat"
+            },
+            {
+              text: "Luaran dan Capaian Tridharma",
+              link: "/data/kriteria-9-luaran-dan-capaian-tridharma-perguruan-tinggi"
+            }
+          ]
+        },
+        {
+          text: "Penutup",
+          collapsible: true,
+          items: [
+            { text: "Penjaminan Mutu", link: "/data/penjaminan-mutu" },
+            {
+              text: "Program Pengembangan Berkelanjutan",
+              link: "/data/program-pengembangan-berkelanjutan"
+            }
+          ]
+        }
+      ]
+    },
+
+    outlineTitle: "Di halaman ini",
+
+    footer: {
+      message: "Aktualisasi Latsar Angkatan 58",
+      copyright: "© 2022 Yasmin Zulfati Yusrina"
+    },
+
+    lastUpdatedText: "Terakhir diperbarui",
+
+    docFooter: {
+      prev: "Kembali",
+      next: "Berikutnya"
+    },
+
+    algolia: {
+      appId: "",
+      apiKey: "",
+      indexName: "apsab"
+    }
   }
 }
