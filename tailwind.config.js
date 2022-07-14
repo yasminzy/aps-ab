08f23ba8575e23ee99dafea13cd6c01f9ddfa92a
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-07-14 09:42:34 +0800

    Use dark mode with class and specify fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  content: ["./docs/.vitepress/**/*.{js,ts,vue}", "./docs/**/*.md"]
+  content: ["./docs/.vitepress/**/*.{js,ts,vue}", "./docs/**/*.md"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      serif: ["Roboto Slab", "serif"],
+      mono: ["Roboto Mono", "monospace"]
+    }
+  }
 }
