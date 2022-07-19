356da33a81a5f271b6d76fce7753fb9b57a9f924
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-07-19 13:56:22 +0800

    Include vue files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  content: ["./docs/.vitepress/**/*.{js,ts,vue}", "./docs/**/*.md"],
+  content: ["./docs/.vitepress/**/*.{js,ts,vue}", "./docs/**/*.{md,vue}"],
+
   darkMode: "class",
+
   theme: {
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
       serif: ["Roboto Slab", "serif"],
       mono: ["Roboto Mono", "monospace"]
+    },
+
+    extend: {
+      colors: {
+        mute: "var(--vp-c-white-mute)",
+        "dark-mute": "var(--vp-c-black-mute)"
+      }
     }
   }
 }
