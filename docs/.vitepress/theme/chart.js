488c616863b62290cfe90aeb42ba02d3d2a54bcc
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-07-20 14:46:04 +0800

    Add default border color

--- docs/.vitepress/theme/chart.js
+++ docs/.vitepress/theme/chart.js
@@ -2,6 +2,7 @@ import { Chart, Legend, Title, Tooltip } from "chart.js"
 
 Chart.register(Legend, Title, Tooltip)
 
+Chart.defaults.borderColor = "rgba(160, 157, 157, 0.25)"
 Chart.defaults.color = "#a09d9f"
 Chart.defaults.font.family = "'Roboto', 'sans-serif'"
 Chart.defaults.font.size = 16
